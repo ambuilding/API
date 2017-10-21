9eb9c23f3410f1765fdf6cc6cb5d3d8b9b7fcee1
ambuilding <explorewo@gmail.com>
2017-10-22 00:07:44 +0800

    Building an API in Laravel
    Responses & Codes. DB structure transformations

---
+++ app/Http/Controllers/LessonController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Lesson;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class LessonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $lessons = Lesson::all();
+
+        return Response::json([
+            'data' => $this->transformCollection($lessons)
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $lesson = Lesson::find($id);
+
+        if (! $lesson) {
+            return Response::json([
+                'error' => [
+                    'message' => 'Lesson does not exist',
+                    'code' => 195 // big enough
+                ]
+            ], 404);
+        }
+
+        return Response::json([
+            'data' => $this->transform($lesson->toArray())
+        ], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    private function transformCollection($lessons)
+    {
+        return array_map([$this, 'transform'], $lessons->toArray());
+    }
+
+    private function transform($lesson)
+    {
+        return [
+            'title' => $lesson['title'],
+            'body' => $lesson['body'],
+            'active' => (boolean) $lesson['active']
+        ];
+    }
+}
